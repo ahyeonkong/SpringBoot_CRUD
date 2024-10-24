6221ff2be57a6dfa8fb4c5dff09719f3c2417289
ahyeonkong <kimah0403@gmail.com>
2024-10-24 22:01:54 +0900

    게시판 상세 조회 API에서 유저 ID를 유저 이름으로 수정

--- backend/src/main/java/com/example/demo/post/dto/PostDetailDTO.java
+++ backend/src/main/java/com/example/demo/post/dto/PostDetailDTO.java
@@ -9,19 +9,19 @@ public class PostDetailDTO {
     상세 페이지 조회를 위한 DTO (PostDetailDTO)
     이 DTO는 서버에서 클라이언트에게 상세 페이지 데이터를 보여줄 때 사용됨
 
-        ex. 서버의 json 응답 (http://localhost:8080/post/6)
+        ex. 서버의 json 응답 (http://localhost:8080/post/8)
         {
-            "title": "제목2",
-            "text": "본문2",
-            "userId": 1
+            "title": "제목3",
+            "text": "본문3",
+            "userName": "akong"
         }
 */
-    private String title;
-    private String text;
-    private Long userId;
+    private final String title;
+    private final String text;
+    private final String userName;
 
     public PostDetailDTO(Post post, String title, String text) {
-        this.userId = post.getUser().getId();
+        this.userName = post.getUser().getName();
         this.title = title;
         this.text = text;
     }
