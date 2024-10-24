ce8948a3b578a64d466b74ba9254e8547023abe5
ahyeonkong <kimah0403@gmail.com>
2024-10-24 21:51:45 +0900

    마이페이지 조회 API에서 유저 ID를 유저 이름으로 수정

--- backend/src/main/java/com/example/demo/post/dto/PostMainPageDTO.java
+++ backend/src/main/java/com/example/demo/post/dto/PostMainPageDTO.java
@@ -9,30 +9,22 @@ public class PostMainPageDTO {
         이 DTO는 서버에서 클라이언트에게 데이터를 보여줄 때 사용됨
 
         ex. 서버의 json 응답
+        title과 userName 데이터 받아오기
         {
             "title": "제목",
-            "userId": 1
+            "userName": "akong"
         },
         {
             "title": "제목2",
-            "userId": 1
+            "userName": "akong"
         }
-        
-        현재 userId로 값을 반환하는데 추후 userName으로 변경할 예정!
     */
 
-    private String title;
-    private Long userId;
-//    private String userName; 추후 사용자 이름 받아올 예정
+    private final String title;
+    private final String userName;
 
-    public PostMainPageDTO(Post post){
+    public PostMainPageDTO(Post post) {
         this.title = post.getTitle();
-        this.userId = post.getUser().getId(); // Post 객체가 User 객체와 연관관계가 있음
+        this.userName = post.getUser().getName(); // Post 객체가 User 객체와 연관관계가 있음
     }
-
-
-//    public PostMainPageDTO(Post post, String userName){
-//        this.title = post.title;
-//        this.userName = userName;
-//    }
 }
